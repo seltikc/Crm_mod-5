adb51da45e563535a3d560a15fc438f77fee9c17
Александр Ситников <you@example.com>
2022-11-30 17:46:51 +0500

    init mode5.2

--- script/crm.js
+++ script/crm.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const formTitle = document.querySelector('.modal__title');
+const form = document.querySelector('.form');
+const closeButton = document.querySelector('.modal__close');
+const discontCheckbox = document.querySelector('.fieldset__input-checkbox');
+const discontInput = document.querySelector('.fieldset__input-discount');
+const priceSum = document.querySelector('.price__span');
+const modalId = document.querySelector('.modal__id');
+
+console.log(formTitle);
+console.log(form);
+console.log(closeButton);
+console.log(discontCheckbox);
+console.log(discontInput);
+console.log(priceSum);
+console.log(modalId);
+
